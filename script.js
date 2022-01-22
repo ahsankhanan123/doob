404c0b898a82bdf732dcc26cf4407ae15caf1285
Ahsan Aman <76155759+ahsankhanan123@users.noreply.github.com>
2022-01-22 00:51:47 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+var toggle = document.querySelector(".toggle");
+var  headerContainer = document.querySelector(" .header-container");
+
+toggle.addEventListener("click", function () {
+ toggle.classList.toggle("active");
+ headerContainer.classList.toggle("active");
+});
+
+
+
+
+
+
+
